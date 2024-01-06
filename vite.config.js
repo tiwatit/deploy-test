9f0e877931673fff7b50c98725bd210e8eae03d1
unknown <arteom.babak@gmail.com>
2024-01-06 11:28:21 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/deploy-test/',
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+
 });
